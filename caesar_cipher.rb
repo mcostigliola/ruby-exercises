0594a57dae0759687a9a6502935e5cca66dc6c61
Marco Costigliola <marco.costigliola@gmail.com>
2020-06-14 22:42:08 +0200

    first commit

---
+++ caesar_cipher.rb
@@ -0,0 +1,20 @@
+require 'pry'
+
+def switch_letter(words, shift)
+    lower = ('a'...'z').to_a
+    upper = ('A'...'Z').to_a
+
+    words = words.split("")
+    ciphred_string = ""
+    words.each do |letter|
+        if upper.include?(letter)
+            ciphred_string += upper[upper.index(letter) + shift]
+        elsif lower.include?(letter)
+            ciphred_string += lower[lower.index(letter) + shift]
+        end
+    end
+
+    puts ciphred_string
+end
+
+switch_letter("Ciao sono marco", 5)
