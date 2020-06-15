257a07bf88667fbb012a6ff2e3832d3047cb5619
Marco Costigliola <marco.costigliola@gmail.com>
2020-06-15 07:14:52 +0200

    Wrapped z char. Managed other char.

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,5 +1,7 @@
 #Caesar's cipher
 
+require 'pry'
+
 def switch_letter(words, shift)
     lower = ('a'...'z').to_a
     upper = ('A'...'Z').to_a
@@ -7,14 +9,22 @@ def switch_letter(words, shift)
     words = words.split("")
     ciphred_string = ""
     words.each do |letter|
-        if upper.include?(letter)
-            ciphred_string += upper[upper.index(letter) + shift]
+        if  letter == 'Z'
+            ciphred_string += upper[shift]
+        elsif letter == 'z'
+            ciphred_string += lower[shift]
+        elsif upper.include?(letter)
+            ciphred_string += upper[(upper.index(letter) + shift) % 26]
         elsif lower.include?(letter)
-            ciphred_string += lower[lower.index(letter) + shift]
+            ciphred_string += lower[(lower.index(letter) + shift) % 26]
+        elsif letter == ' '
+            ciphred_string += ' '
+        else
+            ciphred_string += letter
         end
     end
 
     puts ciphred_string
 end
 
-switch_letter("Ciao sono marco", 5)
+switch_letter("Zz", 5)
