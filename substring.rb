4afea473b29a216a54f8a3006e3b7305e3ea3f0d
Marco Costigliola <marco.costigliola@gmail.com>
2020-06-15 09:51:01 +0200

    implemented if control to check if the key is present and increment the value to 1

--- substring.rb
+++ substring.rb
@@ -1,8 +1,20 @@
-
+require 'pry'
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 #Create function take 2 arguments. 1 word e 1 array of substrings
+def sub_string(word,dictionary)
 #convert string into array with split method
+# word = word.split("")
 #iterate trough the array and see how many times find the word
-
+word_times = Hash[dictionary.collect{|w| [w,""]}]
 #associate the times to a words
+word_times.each_key do |key|
+    if word_times.key?(word.to_s)
+        word_times[word.to_s] = 1
+    end
+end
 #return an hash with the results
+puts word_times
+end
+
+sub_string("below", dictionary)
