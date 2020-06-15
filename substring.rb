8d733133a1bbeffc498a6735e31700ba03b38b96
Marco Costigliola <marco.costigliola@gmail.com>
2020-06-15 07:26:13 +0200

    add pseudocode to resolve the algorhitm

---
+++ substring.rb
@@ -0,0 +1,8 @@
+
+
+#Create function take 2 arguments. 1 word e 1 array of substrings
+#convert string into array with split method
+#iterate trough the array and see how many times find the word
+
+#associate the times to a words
+#return an hash with the results
