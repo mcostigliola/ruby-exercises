cc220be7935f54ff2d9f36cdc1d4c690e7e24d0b
Marco Costigliola <marco.costigliola@gmail.com>
2020-06-16 14:00:46 +0200

    add holder var

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,5 +1,7 @@
 #Take the array with unordered numbers
 numbers = [4,3,78,2,0,2]
+holder = 0
+
 # numbers = [2,3,4,78,1]
 
 #define a bubble sort function
@@ -7,25 +9,22 @@ def bubble_sort(arr_unorder)
     arr_order = Array.new
 
 #iterate the array with two variables
-    arr_unorder.each do |n|
+    arr_unorder.each_with_index do |n,idx|
 
         #take index of iterated array element
-        idx = arr_unorder.index(n).to_i
+        # idx = arr_unorder.index(n).to_i
 
         #compare two variables
         #if the variable[idx] < variable[idx+1] don't change else invert the position
-
-        if arr_unorder[idx] > arr_unorder[idx + 1]
-            arr_order += [arr_unorder[idx + 1], arr_unorder[idx]]
-            p arr_order
-        else
-            arr_order += [n, n[idx + 1]]
-            p arr_order
+        if idx < arr_unorder.length - 1   
+            if n > arr_unorder[idx + 1]
+                holder = n
+                arr_unorder[idx] = arr_unorder[idx + 1]
+                arr_unorder[idx + 1] = holder
+            end
         end
     end
-    p arr_order
+    p arr_unorder
 end    
-
-p numbers
 
 bubble_sort(numbers)
